8f0e2431e9469d227aba0ee57b5cde4c62b0feec
“Kanako_Watanabe” <109854906+watanabekanako@users.noreply.github.com>
2023-06-23 16:28:19 +0900

    修正

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,33 +1,44 @@
 import { TextField } from "@mui/material";
 import React from "react";
-import PrimaryButton from "../../components/elements/Button/Button";
+import { PrimaryButton } from "../../components/elements/Button/Button";
 import axios from "axios";
 const Login = () => {
   const [email, setEmail] = React.useState<string>("");
   const [password, setPassword] = React.useState<string>("");
   const [name, setName] = React.useState<string>("");
   const onLoinButton = () => {
+    if (email.length < 1) {
+      setFormError(true);
+      setMessage("〇〇以上入力してください");
+    }
     axios.post("http://localhost:3000/user", {
       email: email,
       password: password,
       name: name,
     });
   };
+  const [formError, setFormError] = React.useState<boolean>(false);
+  const [message, setMessage] = React.useState<string>();
   return (
     <>
       <TextField
+        sx={{ display: "flex" }}
         value={email}
-        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-          setEmail(e.target.value)
-        }
+        helperText={message}
+        error={formError}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+          setEmail(e.target.value);
+        }}
       />
       <TextField
+        sx={{ display: "flex" }}
         value={password}
         onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
           setPassword(e.target.value)
         }
       />
       <TextField
+        sx={{ display: "flex" }}
         value={name}
         onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
           setName(e.target.value)
